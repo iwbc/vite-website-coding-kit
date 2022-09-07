dd5dc7fe8a0c6965d6b7b401586a38d6c581810f
iwbc <1209760+iwbc@users.noreply.github.com>
2022-09-08 02:31:03 +0900

    基本ビルド環境設定

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,27 @@
+import { resolve } from 'path';
+
+import vitePluginGlobInput from '@macropygia/vite-plugin-glob-input';
+import autoprefixer from 'autoprefixer';
 import { defineConfig } from 'vite';
+import handlebars from 'vite-plugin-handlebars';
+import ViteRestart from 'vite-plugin-restart';
+
+const HBS_VARS = {
+  '/index.html': {
+    content: 'example',
+  },
+};
 
 export default defineConfig({
   root: './src',
+  base: './',
+  server: {
+    host: true,
+    port: 3000,
+  },
   build: {
     outDir: '../dist',
+    emptyOutDir: true,
     rollupOptions: {
       output: {
         assetFileNames: (assetInfo) => {
@@ -13,9 +31,32 @@ export default defineConfig({
           }
           return `assets/${extType}/[name][extname]`;
         },
-        chunkFileNames: 'assets/js/[name].js',
-        entryFileNames: 'assets/js/[name].js',
+        chunkFileNames: (chunkInfo) => {
+          const name = chunkInfo.name.replace(/^src\//, '');
+          return `assets/js/${name}-[hash].js`;
+        },
+        entryFileNames: (chunkInfo) => {
+          const name = chunkInfo.name.replace(/^src\//, '');
+          return `assets/js/${name}-[hash].js`;
+        },
       },
     },
   },
+  css: {
+    postcss: {
+      plugins: [autoprefixer],
+    },
+  },
+  plugins: [
+    vitePluginGlobInput({
+      patterns: ['src/**/*.html', '!src/partials/**/*.html'],
+    }),
+    ViteRestart.default({ restart: ['../vite.config.js'] }),
+    handlebars({
+      partialDirectory: resolve(__dirname, './src' + '/partials'),
+      context(pagePath) {
+        return HBS_VARS[pagePath];
+      },
+    }),
+  ],
 });
