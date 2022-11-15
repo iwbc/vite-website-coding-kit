de39e3479b3097c3e44e7f61fbc293cb27e695d8
iwbc <1209760+iwbc@users.noreply.github.com>
2022-11-15 12:39:51 +0900

    Update svg-sprite

--- vite.config.js
+++ vite.config.js
@@ -1,68 +1,14 @@
-import fs from 'fs';
 import path from 'path';
 
 import globInput from '@macropygia/vite-plugin-glob-input';
 import imagemin from '@macropygia/vite-plugin-imagemin-cache';
 import autoprefixer from 'autoprefixer';
-import glob from 'glob';
-import SVGSpriter from 'svg-sprite';
 import { defineConfig, loadEnv } from 'vite';
 import handlebars from 'vite-plugin-handlebars';
 import ViteRestart from 'vite-plugin-restart';
 
 import handlebarsContext from './handlebars.context';
-
-const svgSprite = ({ inputDir, outputDir }) => {
-  const inputPath = path.resolve(inputDir);
-  const outputPath = path.resolve(outputDir);
-
-  if (!fs.existsSync(inputPath)) {
-    return;
-  }
-
-  const dirents = fs.readdirSync(inputPath, { withFileTypes: true });
-  const dirs = dirents.filter((dirent) => dirent.isDirectory()).map(({ name }) => name);
-  dirs.push(''); // inputディレクトリ直下にsvgファイルが配置されている場合用
-
-  return {
-    name: 'svg-sprite',
-    generateBundle: () => {
-      dirs.forEach((dir) => {
-        const spriteName = dir ? dir : 'sprite';
-        const files = glob.sync(path.join(inputPath, dir) + '/*.svg');
-        const spriter = new SVGSpriter({
-          mode: {
-            symbol: {
-              dest: outputPath,
-              sprite: `${spriteName}.svg`,
-            },
-          },
-          shape: {
-            transform: [
-              {
-                svgo: {
-                  plugins: [{ removeTitle: true }, { removeAttrs: { attrs: 'fill' } }],
-                },
-              },
-            ],
-          },
-        });
-
-        if (files.length) {
-          files.forEach((file) => {
-            spriter.add(file, null, fs.readFileSync(file, 'utf-8'));
-          });
-          spriter.compile((_, result) => {
-            for (const type in result.symbol) {
-              fs.mkdirSync(outputPath, { recursive: true });
-              fs.writeFileSync(result.symbol[type].path, result.symbol[type].contents);
-            }
-          });
-        }
-      });
-    },
-  };
-};
+import svgSprite from './vite/vite-plugin-svg-sprite';
 
 export default defineConfig(({ mode }) => {
   return {
